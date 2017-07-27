ecaaf661cb80b608fd72593b4409e482996c4283
wulabing <hanbing151400@gmail.com>
2017-07-27 23:44:37 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -91,7 +91,7 @@ development_tools_installation(){
 }
 libsodium_installation(){
 	mkdir -p ${libsodium_folder} && cd ${libsodium_folder}
-	wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.13.tar.gz
+	wget https://github.com/jedisct1/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz
 	if [[ ! -f ${libsodium_folder}/libsodium-1.0.13.tar.gz ]]; then
 		echo -e "${Error} libsodium download FAIL"
 		exit 1
