ed7e0925f5a802438a6a951097a8210b62010876
wulabing <hanbing151400@gmail.com>
2017-12-21 13:00:26 +0800

    V4.0 更新

--- shadowsocks_new.sh
+++ shadowsocks_new.sh
@@ -4,12 +4,12 @@
 #	System Request:Debian 7+/Ubuntu 14.04+/Centos 6+
 #	Author:	wulabing
 #	Dscription: SSR glzjin server for manyuser (only)
-#	Version: 3.0
+#	Version: 4.0
 #	Blog: https://www.wulabing.com
 #	Special thanks: Toyo
 #====================================================
 
-sh_ver="3.0"
+sh_ver="4.0"
 libsodium_folder="/etc/libsodium"
 shadowsocks_install_folder="/root"
 supervisor_dir="/etc/supervisor"
@@ -205,6 +205,9 @@ modify_SPEEDTEST(){
 modify_CLOUDSAFE(){
 	sed -i '/CLOUD/c \CLOUDSAFE = '${CLOUDSAFE}'' ${config}
 }
+modify_ANTISSATTACK(){
+	sed -i '/ANTISS/c \ANTISSATTACK = '${ANTISSATTACK}'' ${config}
+}
 modify_MU_SUFFIX(){
 	sed -i '/MU_SUFFIX/c \MU_SUFFIX = '\'${MU_SUFFIX}\''' ${config}
 }
@@ -217,12 +220,27 @@ modify_WEBAPI_URL(){
 modify_WEBAPI_TOKEN(){
 	sed -i '/WEBAPI_TOKEN/c \WEBAPI_TOKEN = '\'${WEBAPI_TOKEN}\''' ${config}
 }
-modify_MYSQL(){
+modify_MYSQL_HOST(){
 	sed -i '/MYSQL_HOST/c \MYSQL_HOST = '\'${MYSQL_HOST}\''' ${config}
+}
+modify_MYSQL_PORT(){
 	sed -i '/MYSQL_PORT/c \MYSQL_PORT = '${MYSQL_PORT}'' ${config}
+}
+modify_MYSQL_USER(){
 	sed -i '/MYSQL_USER/c \MYSQL_USER = '\'${MYSQL_USER}\''' ${config}
+}
+modify_MYSQL_PASS(){
 	sed -i '/MYSQL_PASS/c \MYSQL_PASS = '\'${MYSQL_PASS}\''' ${config}
+}
+modify_MYSQL_DB(){
 	sed -i '/MYSQL_DB/c \MYSQL_DB = '\'${MYSQL_DB}\''' ${config}
+}
+modify_MYSQL(){
+	modify_MYSQL_HOST
+	modify_MYSQL_PASS
+	modify_MYSQL_PORT
+	modify_MYSQL_USER
+	modify_MYSQL_DB
 }
 selectApi(){
 	echo -e "${Yellow} 请选择 API 模式: ${Font}"
@@ -242,8 +260,10 @@ common_set(){
 	stty erase '^H' && read -p "NODE_ID(num_only):" NODE_ID
 	stty erase '^H' && read -p "SPEEDTEST_CIRCLE(num_only,default:0):" SPEEDTEST
 	[[ -z ${SPEEDTEST} ]] && SPEEDTEST="0"
-	stty erase '^H' && read -p "CLOUDSAFE_ON(0 or 1,default:0):" CLOUDSAFE
+	stty erase '^H' && read -p "CLOUDSAFE_ON(0 or 1,default:0,advise:0!):" CLOUDSAFE
 	[[ -z ${CLOUDSAFE} ]] && CLOUDSAFE="0"
+	stty erase '^H' && read -p "ANTISSATTACK(0 or 1,default:0):" ANTISSATTACK
+	[[ -z ${ANTISSATTACK} ]] && ANTISSATTACK="0"
 	stty erase '^H' && read -p "MU_SUFFIX(default:zhaoj.in):" MU_SUFFIX
 	[[ -z ${MU_SUFFIX} ]] && MU_SUFFIX="zhaoj.in"
 	stty erase '^H' && read -p "MU_REGEX(default:%5m%id.%suffix):" MU_REGEX
@@ -313,9 +333,16 @@ SSR_installation(){
 	sleep 1
 }
 
+if_install(){
+	[[ -d ${shadowsocks_folder} && -f ${config} ]] && {
+		echo -e "${OK} ${GreenBG} ShadowsocksR glzjin 已安装 ${Font}"
+	} || {
+		echo -e "${Error} ${RedBG} ShadowsocksR glzjin 未安装，请在安装后执行相关操作 ${Font}"
+		exit 1
+	}
+}
+
 option(){
-	check_system
-	sleep 2
 	echo -e "${Red} 请选择安装内容 ${Font}"
 	echo -e "1. SSR + supervisor"
 	echo -e "2. SSR "
@@ -340,5 +367,213 @@ option(){
 			;;
 	esac
 }
+modify_module(){
+	read -p "请输入 $1 修改内容: " $1
+	modify_$1
+	[[ $? -eq 0 ]] && {
+		echo -e "${OK} ${GreenBG} $1 修改成功 请重新启动后端 ${Font}"
+	} || {
+		echo -e "${Error} ${RedBG} $1 修改失败 ${Font}"
+	}	
+}
+install_management(){
+		check_system
+		echo -e "${Red} 请选择安装内容 ${Font}"
+		echo -e "1. SSR + supervisor"
+		echo -e "2. SSR "
+		echo -e "3. supervisor"
+		read -p "input:" number
+		case ${number} in
+			1)
+				SSR_installation
+				supervisor_installation
+				supervisor_conf_modify_${ID}
+				;;
+			2)
+				SSR_installation
+				;;
+			3)
+				supervisor_installation
+				supervisor_conf_modify_${ID}
+				;;
+			*)
+				echo -e "${Error} ${RedBG} 请输入正确的序号 ${Font}"
+				exit 1			
+				;;
+		esac
+}
+modify_management(){
+	if_install
+	echo -e "${Red}请选择要修改的内容 ${Font}"
+	echo -e "${GreenBG}   公共内容   ${Font}"
+	echo -e "1. NODE_ID（节点编号）"
+	echo -e "2. SPEEDTEST（测速周期）"
+	echo -e "3. CLOUDSAFE（云安全，非常不建议开启）"
+	echo -e "4. ANTISSATTACK（ss攻击抵抗，自动封禁连接方式或密码错误的IP）"
+	echo -e "5. MU_SUFFIX"
+	echo -e "6. MU_REGEX"
+	echo -e	"${GreenBG}   webapi模式相关内容   ${Font}"
+	echo -e "7. WEBAPI_URL"	
+	echo -e "8. WEBAPI_TOKEN" 
+	echo -e "${GreenBG}   glzjinmod模式相关内容   ${Font}"
+	echo -e "9. MYSQL_HOST（数据库主机）"
+	echo -e "10.MYSQL_PORT（数据库端口）"
+	echo -e "11.MYSQL_USER（数据库用户名）"
+	echo -e "12.MYSQL_PASSWORD（数据库密码）"
+	echo -e "13.MYSQL_DB（数据库名称）"
+	read -p "input:" modify
+	case ${modify} in 
+		1)
+			modify_module "NODE_ID"
+			;;
+		2)
+			modify_module "SPEED_TEST"
+			;;
+		3)
+			modify_module "CLOUDSAFE"
+			;;
+		4)
+			modify_module "ANTISSATTACK"
+			;;
+		5)
+			modify_module "MU_SUFFIX"
+			;;
+		6)
+			modify_module "MU_REGEX"
+			;;
+		7)
+			modify_module "WEBAPI_URL"
+			;;
+		8)
+			modify_module "WEBAPI_TOKEN"
+			;;
+		9)
+			modify_module "MYSQL_HOST"
+			;;
+		10)
+			modify_module "MYSQL_PORT"
+			;;
+		11)
+			modify_module "MYSQL_USER"
+			;;
+		12)
+			modify_module "MYSQL_PASS"
+			;;
+		13)
+			modify_module "MYSQL_DB"
+			;;
+		*)
+			echo -e "${RedBG} 请输入正确的序号 ${Font}"
+			exit 1
+			;;
+	esac
+}
+uninstall_management(){
+	if_install
+	rm -rf ${shadowsocks_folder}
+	echo -e "${OK$ {GreenBG} shadowsocks glzjin 卸载完成 ${Font}"
+	exit 0
+}
+start_management(){
+	command -v supervisord >/dev/null
+	if [[ $? -ne 0  ]];then
+		echo -e "${Notification} 检测到未安装 supervisord"
+		/root/shadowsocks/logrun.sh
+		echo -e "${OK} ${GreenBG} 后端已启动 ${Font}"
+	else
+		echo -e "${OK} 检测到已安装 supervisord"
+		command -v systemctl >/dev/null
+		if [[  $? -ne 0 ]];then
+			service supervisord start
+			[[ `ps -ef | grep supervisor |grep -v grep | wc -l` -ge "0" ]] && {
+				echo -e "${OK} ${GreenBG} 后端（supervisord）已启动 ${Font}"
+			} || {
+				echo -e "${Error} ${RedBG} 后端启动失败 ${Font}"
+				exit 1
+			}
+		else
+			systemctl start supervisor
+			[[ `ps -ef | grep supervisor |grep -v grep | wc -l` -ge "0" ]] && {
+				echo -e "${OK} ${GreenBG} 后端（supervisord）已启动 ${Font}"
+			} || {
+				echo -e "${Error} ${RedBG} 后端启动失败 ${Font}"
+				exit 1
+			}
+		fi
+	fi
 
-option
+}
+stop_management(){
+	command -v supervisord >/dev/null
+	if [[ $? -ne 0 ]];then
+		echo -e "${Notification} 检测到未安装 supervisord"
+		/root/shadowsocks/stop.sh
+		echo -e "${OK} ${GreenBG} 后端已关闭 ${Font}"
+	else
+		echo -e "${OK} 检测到已安装 supervisord"
+		command -v systemctl >/dev/null
+		if [[ $? -ne 0 ]];then
+			service supervisord stop
+			[[ `ps -ef | grep supervisor |grep -v grep | wc -l` -eq "0" ]] && {
+				echo -e "${OK} ${GreenBG} 后端（supervisord）已关闭 ${Font}"
+			} || {
+				echo -e "${Error} ${RedBG} 后端关闭失败 ${Font}"
+				exit 1
+			}
+		else
+			systemctl stop supervisor
+			[[ `ps -ef | grep supervisor |grep -v grep | wc -l` -eq "0" ]] && {
+				echo -e "${OK} ${GreenBG} 后端（supervisord）已关闭 ${Font}"
+			} || {
+				echo -e "${Error} ${RedBG} 后端关闭失败 ${Font}"
+				exit 1
+			}
+		fi
+	fi
+}
+force_stop(){
+	supervisor_pid=` ps -ef | grep supervisor |grep -v grep|awk '{print $2}'`
+	ss_pid=` ps -ef | grep server.py |grep -v grep|awk '{print $2}' `
+	kill -9 ${supervisor_pid} ${ss_pid}
+	echo -e "${OK} ${GreenBG} 后端（supervisord）已关闭 ${Font}"
+}
+management(){
+	case $1 in
+		install)
+			install_management
+			;;
+		modify)
+			modify_management
+			;;
+		uninstall)
+			uninstall_management
+			;;
+		start)
+			start_management
+			;;
+		stop)
+			stop_management
+			;;
+		restart)
+			stop_management
+			start_management
+			;;
+		fstop)
+			force_stop
+			;;
+		status)
+			if [[ `ps -ef | grep server.py |grep -v grep | wc -l` -ge "1" ]];then
+				echo -e "${OK} ${GreenBG} 后端已启动 ${Font}"
+			else
+				echo -e "${OK} ${RedBG} 后端未启动 ${Font}"
+				exit 1
+			fi
+			;;
+		*)
+			echo -e "${Notification} Usage:{start|stop|fstop|status|install|uninstall|modify}"
+			exit 1
+			;;
+	esac
+}
+management $1
+
